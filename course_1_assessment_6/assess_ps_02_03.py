b6051d939147143dbab49c2666f351cd83a0a1c0
Dan Chamorro <i@danchamorro.com>
2021-10-16 21:40:49 -0400

    completed assess 2.3

---
+++ course_1_assessment_6/assess_ps_02_03.py
@@ -0,0 +1,8 @@
+# Write code that uses iteration to print out the length of each element of the list stored in str_list.
+
+str_list = ["hello", "", "goodbye", "wonderful", "I love Python"]
+
+# Write your code here.
+
+for i in str_list:
+    print(len(i))
