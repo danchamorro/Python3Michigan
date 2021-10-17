a2243344d2292ad00369f107a15c4da04449c713
Dan Chamorro <i@danchamorro.com>
2021-10-16 22:36:10 -0400

    completed assess 2.10

---
+++ course_1_assessment_6/assess_pc_02_10.py
@@ -0,0 +1,10 @@
+# Create an empty string and assign it to the variable lett. Then using range, write code such that when your code is run,
+# lett has 7 b’s ("bbbbbbb").
+
+lett = ""
+
+for i in range(7):
+    i = "b"
+    lett = lett + i
+
+print(lett)
