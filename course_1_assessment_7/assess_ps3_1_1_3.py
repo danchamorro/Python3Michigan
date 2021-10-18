7c8d295806ca7ae895f424cb0f0de9495f1bc708
Dan Chamorro <i@danchamorro.com>
2021-10-17 20:28:40 -0400

    completed asses 3.1.1.3

---
+++ course_1_assessment_7/assess_ps3_1_1_3.py
@@ -0,0 +1,17 @@
+# Write code to count the number of strings in list items that have the character w in it. Assign that number to the
+# variable acc_num.
+
+# HINT 1: Use the accumulation pattern!
+
+# HINT 2: the in operator checks whether a substring is present in a string.
+
+# Hard-coded answers will receive no credit.
+
+items = ["whirring", "wow!", "calendar", "wry",
+         "glass", "", "llama", "tumultuous", "owing"]
+
+acc_num = 0
+
+for i in items:
+    if "w" in i:
+        acc_num += 1
