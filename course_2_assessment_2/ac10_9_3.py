0e8886b3ba3a35df5a02ebe2b2057b8309dd46ea
Dan Chamorro <i@danchamorro.com>
2021-10-20 13:33:17 -0400

    completed assignment 10.9.3

---
+++ course_2_assessment_2/ac10_9_3.py
@@ -0,0 +1,9 @@
+# Add the string “hockey” as a key to the dictionary sports_periods and assign it the value of 3.
+# Do not rewrite the entire dictionary.
+
+
+sports_periods = {'baseball': 9, 'basketball': 4, 'soccer': 4, 'cricket': 2}
+
+sports_periods["hockey"] = 3
+
+print(sports_periods)
