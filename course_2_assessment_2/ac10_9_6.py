f8a339d5d13599087d10f600cc5ab2c8d16cba42
Dan Chamorro <i@danchamorro.com>
2021-10-20 13:48:55 -0400

    completed assignment 10.9.6

---
+++ course_2_assessment_2/ac10_9_6.py
@@ -0,0 +1,11 @@
+# Provided is the dictionary, medal_count, which lists countries and their respective medal count at the halfway point in
+# the 2016 Rio Olympics. Using dictionary mechanics, assign the medal count value for "Belarus" to the variable belarus.
+# Do not hardcode this.
+
+
+medal_count = {'United States': 70, 'Great Britain': 38, 'China': 45, 'Russia': 30, 'Germany': 17, 'Italy': 22, 'France': 22, 'Japan': 26, 'Australia': 22, 'South Korea': 14, 'Hungary': 12, 'Netherlands': 10, 'Spain': 5, 'New Zealand': 8, 'Canada': 13, 'Kazakhstan': 8, 'Colombia': 4, 'Switzerland': 5, 'Belgium': 4,
+               'Thailand': 4, 'Croatia': 3, 'Iran': 3, 'Jamaica': 3, 'South Africa': 7, 'Sweden': 6, 'Denmark': 7, 'North Korea': 6, 'Kenya': 4, 'Brazil': 7, 'Belarus': 4, 'Cuba': 5, 'Poland': 4, 'Romania': 4, 'Slovenia': 3, 'Argentina': 2, 'Bahrain': 2, 'Slovakia': 2, 'Vietnam': 2, 'Czech Republic': 6, 'Uzbekistan': 5}
+
+belarus = medal_count["Belarus"]
+
+print(belarus)
