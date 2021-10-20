cd522eb70b8a4bdc7c81f93ee74dc1c6572554ad
Dan Chamorro <i@danchamorro.com>
2021-10-20 14:07:59 -0400

    completed assignment 10.9.7

---
+++ course_2_assessment_2/ac10_9_7.py
@@ -0,0 +1,11 @@
+# The dictionary total_golds contains the total number of gold medals that countries have won over the course of history.
+# Use dictionary mechanics to find the number of golds Chile has won, and assign that number to the variable name chile_golds.
+# Do not hard code this!
+
+
+total_golds = {"Italy": 114, "Germany": 782, "Pakistan": 10, "Sweden": 627, "USA": 2681, "Zimbabwe": 8, "Greece": 111, "Mongolia": 24, "Brazil": 108, "Croatia": 34, "Algeria": 15, "Switzerland": 323, "Yugoslavia": 87, "China": 526, "Egypt": 26,
+               "Norway": 477, "Spain": 133, "Australia": 480, "Slovakia": 29, "Canada": 22, "New Zealand": 100, "Denmark": 180, "Chile": 13, "Argentina": 70, "Thailand": 24, "Cuba": 209, "Uganda": 7,  "England": 806, "Denmark": 180, "Ukraine": 122, "Bahamas": 12}
+
+chile_golds = total_golds["Chile"]
+
+print(chile_golds)
