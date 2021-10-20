b821c96a9afaf404c73d7f6ee1e9eabdbe956631
Dan Chamorro <i@danchamorro.com>
2021-10-20 13:31:33 -0400

    completed assignment 10.9.2

---
+++ course_2_assessment_2/ac10_9_2.py
@@ -0,0 +1,9 @@
+# Given the dictionary swimmers, add an additional key-value pair to the dictionary with "Phelps" as the key and the integer 23
+# as the value. Do not rewrite the entire dictionary.
+
+
+swimmers = {'Manuel': 4, 'Lochte': 12, 'Adrian': 7, 'Ledecky': 5, 'Dirado': 4}
+
+swimmers["Phelps"] = 23
+
+print(swimmers)
