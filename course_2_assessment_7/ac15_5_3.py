158b8853f7a71643611714b32f6d86633c41212f
Dan Chamorro <i@danchamorro.com>
2021-10-25 23:01:51 -0400

    completed 15.5.3

---
+++ course_2_assessment_7/ac15_5_3.py
@@ -0,0 +1,5 @@
+# Below is a function, sum, that does not work. Change the function definition so the code works.
+# The function should still have a required parameter, intx, and an optional parameter, intz with a defualt value of 5.
+
+def sum(intx, intz=5):
+    return intz + intx
