ed72d3d75cc93050977f044c55831d7dfa4997f3
Dan Chamorro <i@danchamorro.com>
2021-10-27 22:47:05 -0400

    completed assesement 18.7.2

---
+++ course_2_assessment_8/ac18_7_2.py
@@ -0,0 +1,8 @@
+# Sort the list below, animals, into alphabetical order, a-z. Save the new list as animals_sorted.
+
+animals = ['elephant', 'cat', 'moose', 'antelope', 'elk', 'rabbit', 'zebra', 'yak',
+           'salamander', 'deer', 'otter', 'minx', 'giraffe', 'goat', 'cow', 'tiger', 'bear']
+
+animals_sorted = sorted(animals)
+
+print(animals_sorted)
