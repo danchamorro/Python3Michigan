48e3d47c1484b6544b83c41035d5f2d50957c7a9
Dan Chamorro <i@danchamorro.com>
2021-10-27 22:45:21 -0400

    completed assesement 18.7.1

---
+++ course_2_assessment_8/ac18_7_1.py
@@ -0,0 +1,7 @@
+# Sort the following string alphabetically, from z to a, and assign it to the variable sorted_letters.
+
+letters = "alwnfiwaksuezlaeiajsdl"
+
+sorted_letters = sorted(letters, reverse=True)
+
+print(sorted_letters)
