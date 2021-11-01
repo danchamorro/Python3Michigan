569d7b80dbbdc12b25be46303437f9c0ce408451
Dan Chamorro <i@danchamorro.com>
2021-10-31 20:13:09 -0400

    completed assesment 17.1.5

---
+++ CheckMyUnderstanding/ListWithComplexItems17_1_1/ac17_1_5.py
@@ -0,0 +1,9 @@
+# Below, we have provided a list of lists. Use indexing to assign the element ‘horse’ to the variable name idx1
+
+
+animals = [['cat', 'dog', 'mouse'], [
+    'horse', 'cow', 'goat'], ['cheetah', 'giraffe', 'rhino']]
+
+idx1 = animals[1][0]
+
+print(idx1)
