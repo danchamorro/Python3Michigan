f4280092b22234010da3ab10517ada3c40164b30
Dan Chamorro <i@danchamorro.com>
2021-10-31 21:33:34 -0400

    completed assesment 17.4.2

---
+++ CheckMyUnderstanding/NestedIteration17_4/ac17_4_2.py
@@ -0,0 +1,12 @@
+# Below, we have provided a list of lists that contain information about people.
+# Write code to create a new list that contains every person’s last name, and save that list as last_names.
+
+
+info = [['Tina', 'Turner', 1939, 'singer'], ['Matt', 'Damon', 1970, 'actor'], ['Kristen', 'Wiig',
+                                                                               1973, 'comedian'], ['Michael', 'Phelps', 1985, 'swimmer'], ['Barack', 'Obama', 1961, 'president']]
+last_names = []
+
+for lst in info:
+    last_names.append(lst[1])
+
+print(last_names)
