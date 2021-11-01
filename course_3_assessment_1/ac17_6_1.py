c4ab5e00287ec5194bbfdafd5d71a0d51911885d
Dan Chamorro <i@danchamorro.com>
2021-10-31 22:05:01 -0400

    completed assesment 17.6.1

---
+++ course_3_assessment_1/ac17_6_1.py
@@ -0,0 +1,9 @@
+# The variable nested contains a nested list. Assign ‘snake’ to the variable output using indexing.
+
+
+nested = [['dog', 'cat', 'horse'], ['frog', 'turtle',
+                                    'snake', 'gecko'], ['hamster', 'gerbil', 'rat', 'ferret']]
+
+output = nested[1][2]
+
+print(output)
