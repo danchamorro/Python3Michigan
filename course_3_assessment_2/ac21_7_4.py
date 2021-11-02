5e553776abb0c8fe5d80c1a5beb94e812534f113
Dan Chamorro <i@danchamorro.com>
2021-11-02 17:05:45 -0400

    completed assesment 21.7.4

---
+++ course_3_assessment_2/ac21_7_4.py
@@ -0,0 +1,7 @@
+# Use list comprehension to create a list called lst2 that doubles each element in the list, lst.
+
+lst = [["hi", "bye"], "hello", "goodbye", [9, 2], 4]
+
+lst2 = [elm * 2 for elm in lst]
+
+print(lst2)
