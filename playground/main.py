ad2d613a9cc16ad5f6c3e4cc71da81279c363c73
Dan Chamorro <i@danchamorro.com>
2021-11-06 11:06:34 -0400

    messing with flikr api

--- playground/main.py
+++ playground/main.py
@@ -10,4 +10,18 @@ def connect_to_itunes(artist):
     return json.dumps(data, indent=2)  # pretty print
 
 
-print(connect_to_itunes('taylor swift'))
+# print(connect_to_itunes('taylor swift'))
+
+# Connect to flikr api
+def connect_to_flikr(keyword):
+    import requests
+    import json
+    url = 'https://api.flickr.com/services/rest'  # url to search
+    params = {'method': 'flickr.photos.search', 'api_key': '2f5ac274ecfac5a455f38745704ad084',
+              'text': keyword, 'format': 'json', 'nojsoncallback': '1', 'extras': 'url_m'}  # parameters to search
+    r = requests.get(url, params=params)  # request
+    data = r.json()  # convert to json
+    return json.dumps(data, indent=2)  # pretty print
+
+
+print(connect_to_flikr('taylor swift'))
